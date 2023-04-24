3083f8a32296ac7d4f4c21389808d9c468085a3e
GregPtto <119642298+GregPtto@users.noreply.github.com>
2023-04-24 21:07:04 +0400

    Update pour init les variables dans pendu.c

--- pendu.c
+++ pendu.c
@@ -1,37 +1,79 @@
 #include <stdio.h>
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+char* init_mot(){
+    char *mot = "maison";
+    return mot;
+}
+
+void init_game_test(char *mot_a_trouver){
+    char *mot = init_mot();
+    strcpy(mot_a_trouver,mot);
+}
+
+
+char* init_mot_troue(int entier){
+    char essai[entier];
+    for(int i = 0; i < entier; i++){
+        essai[i] = '_';
+    }
+    return essai;
+}
+
 void init_game(char *mot, int *nb_carac_mot, int *nb_essais, char *coeurs,char *essai){
-    mot = malloc(5*sizeof(char));
-    essai = malloc(5*sizeof(char)); 
-    // image_pendu = malloc(73*sizeof(char));
-    nb_essais = malloc(sizeof(int));
-    nb_carac_mot = malloc(sizeof(int));
+
+    strcpy(mot,init_mot());
+
+    *nb_carac_mot = strlen(mot);
+
     *nb_essais = 5;
-    coeurs = malloc(*nb_essais*sizeof(char));
-    coeurs = "♥♥♥♥♥";
-    mot = "maison";
-    *nb_carac_mot = 6;
-    
-    // printf("%s\n", image_pendu);
+
+    strcpy(coeurs,"00000");
 
     for(int i = 0; i < *nb_carac_mot; i++){
         essai[i] = '_';
     }
-    
+    //strcpy(essai,init_mot_troue(*nb_carac_mot));
+
+}
+
+void fin_du_game(char *message_fin, int resultat){
+    if(resultat){
+       strcpy(message_fin, "Fin du jeu, GAGNE\n");
+    } else{
+        strcpy(message_fin, "Fin du jeu, PERDU\n");
+    }
 }
 
 
 
 
 
-void fin_du_game(char *message_fin, int resultat){
-    message_fin = malloc(sizeof(char)*18);
+int main(){
+    char mot[100];
+    int nb_carac_mot,nb_essai;
+    char essais[20];
+    char coeurs[20];    
+    char message_fin[18];
 
-    if(resultat){
-        message_fin = "Fin du jeu, GAGNE\n";
-    } else{
-        message_fin = "Fin du jeu, PERDU\n";
-    }
+    init_game(mot,&nb_carac_mot,&nb_essai,coeurs,essais);
+    printf("Le mot est : %s\n",mot);
+    printf("Il y a %d lettre dans le mot \n",nb_carac_mot);
+    printf("Il a y %d essais pour trouver le mot\n",nb_essai);
+    printf("Voici la liste de coeurs : %s\n",coeurs);
+    printf("Voici le mot sans les lettres %s\n",essais);
+
+    fin_du_game(message_fin,1);
+    printf("Le message de fin est : %s",message_fin);
+
+
+
+    return 0;
 }
 
